0fe2db4a8f91fe70fca039796a64aed50850629a
aoz69 <aozpokharel@gmail.com>
2022-06-06 17:52:37 +0545

    register button works

--- app/src/main/java/com/example/casebook/MainActivity.java
+++ app/src/main/java/com/example/casebook/MainActivity.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button submit = findViewById(R.id.submit);
+        register = findViewById(R.id.RegHere);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reg();
+            }
+        });
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void reg() {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
     }
 
     private void change() {
--- app/src/main/java/com/example/casebook/database.java
+++ app/src/main/java/com/example/casebook/database.java
@@ -2,6 +2,7 @@ package com.example.casebook;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -28,7 +29,7 @@ public class database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
-        String q = " CREATE TABLE " +table +
+        String q = " CREATE TABLE USERS" +
                 "(" + for_name + " TEXT ," + for_email + " TEXT, " + for_pass  + " TEXT )";
 
         db.execSQL(q);
@@ -38,7 +39,6 @@ public class database extends SQLiteOpenHelper {
     public void addUsers(String email, String name , String password){
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues data = new ContentValues();
-
         data.put(for_email , email);
         data.put(for_name , name);
         data.put(for_pass , password);
@@ -46,9 +46,27 @@ public class database extends SQLiteOpenHelper {
         database.insert(table , null , data);
         database.close();
     }
+
+
+    public Cursor showUsers(String email) { //ID ID ID
+        SQLiteDatabase database = this.getReadableDatabase();
+        return database.rawQuery("SELECT * FROM USERS WHERE email = ?" , new String[] {email});
+    }
+
+
+
+    /*
+    cursor lai read garna
+    database db = new database(this);
+    Cursor cursor = db.showUsers(String email);
+    (VARIABLE)cursor.movetofirst();
+    name = cursor.getString(cursor.getColulmnIndexOrThrow("Name"); //name leu
+    */
+
+
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-        // this method is called to check if the table exists already.
+        // check if the table already exists nad drop if it does
         database.execSQL("DROP TABLE IF EXISTS " +table);
         onCreate(database);
     }
